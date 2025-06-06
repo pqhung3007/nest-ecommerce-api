3cbc8a5e8fc17015d7b9051dd46f3fe81d6cdcbf
pqhung3007 <hungpham307@gmail.com>
2025-06-06 09:49:02 +0700

    fix: review module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { Product } from '../products/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review, Product])],
   providers: [ReviewsService],
-  controllers: [ReviewsController]
+  controllers: [ReviewsController],
+  exports: [ReviewsService],
 })
 export class ReviewsModule {}
